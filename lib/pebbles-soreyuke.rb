e3511a136458df68b894905142d239924bf544d6
fukayatsu <fukayatsu@gmail.com>
2012-06-06 00:49:04 +0900

    first commit

---
+++ lib/pebbles-soreyuke.rb
@@ -0,0 +1,7 @@
+require "pebbles-soreyuke/version"
+
+module Pebbles
+  module Soreyuke
+    # Your code goes here...
+  end
+end
---
+++ lib/pebbles-soreyuke/version.rb
@@ -0,0 +1,5 @@
+module Pebbles
+  module Soreyuke
+    VERSION = "0.0.1"
+  end
+end
