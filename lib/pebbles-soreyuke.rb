94d5d07091f9b273de39853e7a79653cdf182c64
fukayatsu <fukayatsu@gmail.com>
2012-12-09 19:50:19 +0900

    add cpm and spm

--- lib/pebbles-soreyuke.rb
+++ lib/pebbles-soreyuke.rb
@@ -1,7 +1,60 @@
+# -*- encoding: utf-8 -*-
 require "pebbles-soreyuke/version"
 
 module Pebbles
   module Soreyuke
-    # Your code goes here...
+
+    module_function
+
+    def AA(caracter, text)
+      case caracter
+      when 'apm'
+        apm(text)
+      when 'cpm'
+        cpm(text)
+      when 'spm'
+        spm(text)
+      end
+    end
+
+    def apm(text = nil)
+      text ||= "I am APM!"
+      <<"EOS"
+         ＿＿＿
+      ／       ＼         ＿＿＿＿＿＿＿＿
+     /   ∧   ∧   ＼     ／
+    |    ・  ・   |  ＜   #{text}
+    |     ）●（   |    ＼＿＿＿＿＿＿＿＿
+    ＼    ー     ﾉ
+      ＼＿＿＿_／
+EOS
+    end
+
+    def cpm(text = nil)
+      text || "I am CPM!"
+      <<"EOS"
+        ＿＿ヘ＿ヘ＿          ＿＿＿＿＿＿＿＿
+      ／∴∵(・) (・)∴＼      ／
+     /∴∵∴∵    ○   ∴∵ ヽ   ＜   #{text}
+    (∵∴∵  ●～～～●  ∴ )     ＼＿＿＿＿＿＿＿＿
+     ＼∵∴∵∴∵∴∵     ／
+       ＼∵∴∵∴    ／
+EOS
+    end
+
+    def spm(text = nil)
+      text || "I am SPM!"
+      <<"EOS"
+      ＿＿＿＿
+    ／          ＼
+  (               ）    ＿＿＿＿＿＿＿＿
+   ＼   ∧   ∧    |    ／
+    |   ・  ・   |  ＜　   #{text}
+    |  ○  ||  ○. |    ＼＿＿＿＿＿＿＿＿
+    |     凵     |
+    |     ー     |
+    |＿＿＿＿＿＿|
+EOS
+    end
   end
 end
--- lib/pebbles-soreyuke/version.rb
+++ lib/pebbles-soreyuke/version.rb
@@ -1,5 +1,5 @@
 module Pebbles
   module Soreyuke
-    VERSION = "0.0.1"
+    VERSION = "0.1.1"
   end
 end
